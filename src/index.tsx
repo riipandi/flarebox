b0fa7edfb72da9de193db61a54856845d155c75a
Aris Ripandi <riipandi@gmail.com>
2026-05-01 06:35:42 +0700

    refactor: reorganize code structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import { Hono } from 'hono'
+import HomePage from '@/pages/home'
 import { renderer } from './renderer'
-import { Hero01 } from '@/views/hero-01'
 
 const app = new Hono()
 
 app.use(renderer)
 
 app.get('/', (c) => {
-  return c.render(<Hero01 image={{src: 'https://kiwaui.com/public/screenshots/dashboard-overview-inset.png', alt:'Hero'}} />)
+  return c.render(<HomePage />)
 })
 
 export default app
---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+import { Hero01 } from '@/pages/-home/section-hero'
+
+export default function Page() {
+  return (
+    <div>
+      <Hero01 image={{src: 'https://kiwaui.com/public/screenshots/dashboard-overview-inset.png', alt:'Hero'}} />
+    </div>
+  )
+}
